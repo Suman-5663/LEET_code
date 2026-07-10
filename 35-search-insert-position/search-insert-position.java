0918b598a65d3dad1fda228a2b547d59bc2b3cad
Suman <sumandash5663@gmail.com>
2026-07-10 12:34:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (28.66%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,33 +1,34 @@
-// class Solution {
-//     public int searchInsert(int[] nums, int target) {
-//         int n = nums.length;
-//           if(nums[n-1] == target){
-//             return n-1;
-//         }
-//      for(int i=0;i<n-1;i++){
-//         if(nums[i] == target){
-//             return i;
-//         }
-//         if(nums[i]< target && nums[i+1] > target){
-//             return i+1;
-//         }
-//       }
-//       return -1;
-//     }
-// }
-
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        if (n == 0) return 0;
+        if(nums[n-1] == target) return n-1;
         if (target<nums[0]) return 0;
         if (target>nums[n-1]) return n;
         int i=0;
-        while (i<n){
-            if ( i<n-1 && nums[i]<target && nums[i+1]>target) return i+1;
-            if (target==nums[i]) return i;
-            i++;
+        for(i=0;i<n-1;i++){
+            if(nums[i] == target){
+            return i;}
+        if(nums[i]< target && nums[i+1] > target){
+            return i+1;
         }
-        return 0;
+        }
+        return -1;
     }
+
 }
+
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+//         int n = nums.length;
+//         if (n == 0) return 0;
+//         if (target<nums[0]) return 0;
+//         if (target>nums[n-1]) return n;
+//         int i=0;
+//         while (i<n){
+//             if ( i<n-1 && nums[i]<target && nums[i+1]>target) return i+1;
+//             if (target==nums[i]) return i;
+//             i++;
+//         }
+//         return 0;
+//     }
+// }
