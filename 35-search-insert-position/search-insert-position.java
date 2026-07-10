bba6350bb6d2caf021fc4a9e7b93a8717d9b3db0
Suman <sumandash5663@gmail.com>
2026-07-10 12:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.02%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,11 +1,11 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        if(nums[n-1] == target) return n-1;
+        // if(nums[n-1] == target) return n-1;
         if (target<nums[0]) return 0;
         if (target>nums[n-1]) return n;
         int i=0;
-        for(i=0;i<n-1;i++){
+        for(i=0;i<n;i++){
             if(nums[i] == target){
             return i;}
         if(nums[i]< target && nums[i+1] > target){
